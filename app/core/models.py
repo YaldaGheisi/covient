63d6b7390321752d37cd6bfec12557a60f6846ca
YaldaGheisi <89938223+YaldaGheisi@users.noreply.github.com>
2023-02-16 10:04:12 -0600

    add test email

--- app/core/models.py
+++ app/core/models.py
@@ -12,9 +12,9 @@ from django.contrib.auth.models import(
 class UserManager(BaseUserManager):
     """manager for users."""
 
-    def create_user(self, email, password=None, **extra_field):
+    def create_user(self, email, password=None, **extra_fields):
         """create, save and return a new user."""
-        user = self.model(email=email, **extra_field)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
@@ -29,6 +29,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
-    object = UserManager()
+    objects = UserManager()
 
     USERNAME_FIELD = 'email'
