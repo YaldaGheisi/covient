a968912fc92cf99b255257e8650847f815780777
YaldaGheisi <89938223+YaldaGheisi@users.noreply.github.com>
2023-03-08 13:13:20 -0600

    fixed

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -22,6 +22,17 @@ class UserSerializer(serializers.ModelSerializer):
         """Create and return a user with encrypted password."""
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """Update and return user."""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
     """Serializer for the user auth token."""
@@ -33,8 +44,8 @@ class AuthTokenSerializer(serializers.Serializer):
 
     def validate(self, attrs):
         """validate and authenticate the user."""
-        email = attrs.get('email'),
-        password = attrs.get('password'),
+        email = attrs.get('email')
+        password = attrs.get('password')
         user = authenticate(
             request=self.context.get('request'),
             username=email,
@@ -42,7 +53,7 @@ class AuthTokenSerializer(serializers.Serializer):
         )
         if not user:
             msg = _('Unable to authenticate with provided credentials')
-            raise serializers.ValidationEroor(msg, code='authorization')
+            raise serializers.ValidationError(msg, code='authorization')
 
         attrs['user'] = user
         return attrs
